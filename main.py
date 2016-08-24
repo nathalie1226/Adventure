f55f6893027fc955db8cd1414321d8a78ca61581
nathalie <nathalie1226@hotmail.com>
2016-08-24 14:47:28 +0300

    refactoring

--- main.py
+++ main.py
@@ -4,10 +4,119 @@ import json
 import pymysql
 import os
 
+# connection = pymysql.connect(host="us-cdbr-iron-east-04.cleardb.net",
+#                              user="b69ece0fa2a81b",
+#                              password="58cea4d5",
+#                              db="heroku_d958e16ebe26a90",
+#                              charset="utf8",
+#                              cursorclass=pymysql.cursors.DictCursor)
+
+
+connection = pymysql.connect(host="localhost",
+                             user="root",
+                             password="",
+                             db="adventure",
+                             charset="utf8",
+                             cursorclass=pymysql.cursors.DictCursor)
+
 @route("/", method="GET")
 def index():
     return template("adventure.html")
+#
+# def selectUserInfo(username):
+#     with connection.cursor() as cursor:
+#         sql = "SELECT * FROM users WHERE var1=%s"% (username)
+#         # sql = """SELECT ValA FROM %s WHERE Val2 = %s AND Val3 = %s""" % (Val1, Val2, Val3)
+#         cursor.execute(sql,username)
+#         user_info = cursor.fetchone()
+#         print(user_info)
+#         return user_info
 
+def selectAllUserInfoFromName(username):
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM users WHERE username='{}'".format(username)
+        cursor.execute(sql)
+        user_info = cursor.fetchone()
+        print(user_info)
+        return user_info
+
+def selectAllUserInfoFromID(user_id):
+    with connection.cursor() as cursor:
+        sql = "SELECT * from users where user_id='{}'".format(user_id)
+        cursor.execute(sql)
+        user_info = cursor.fetchone()
+        print(user_info)
+        return user_info
+
+def selectUserIdFromName(username):
+    with connection.cursor() as cursor:
+        sql7 = "SELECT user_id from users WHERE username='{}'".format(username)
+        cursor.execute(sql7)
+        result7 = cursor.fetchone()
+        user_id = result7["user_id"]
+        print(result7)
+        return user_id
+
+def selectCurrentQuestion(user_id):
+    with connection.cursor() as cursor:
+        sql2 = "SELECT current_question FROM users WHERE user_id='{}'".format(user_id)
+        cursor.execute(sql2)
+        cq = cursor.fetchone()
+        question_id = cq["current_question"]
+        print("the current question of the user is")
+        print(question_id)
+        return question_id
+
+def selectQuestionText(question_id):
+    with connection.cursor() as cursor:
+        sql3 = "SELECT question_text from questions where question_id='{}'".format(question_id)
+        cursor.execute(sql3)
+        result3 = cursor.fetchone()
+        text = result3["question_text"]
+        print(text)
+        return text
+
+
+
+def selectQuestionOptions(question_id):
+    with connection.cursor() as cursor:
+        sql4 = "SELECT option_id,option_text from options where question_id='{}' ORDER BY option_id ASC".format(
+            question_id)
+        cursor.execute(sql4)
+        result4 = cursor.fetchall()
+        print(result4)
+        return result4
+
+def selectNextQuestionId(question_id,next_story_id):
+    with connection.cursor() as cursor:
+        sql = "SELECT next_question_id from options WHERE question_id='{}' and option_id='{}'".format(question_id,
+                                                                                                      next_story_id)
+
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        print(result["next_question_id"])
+        nq = result["next_question_id"]
+        return nq
+
+def selectGoldLifeChanges(question_id,next_story_id):
+    with connection.cursor() as cursor:
+        sql5 = "SELECT gold_change,life_change from options where question_id='{}' and option_id='{}'".format(
+            question_id, next_story_id)
+        cursor.execute(sql5)
+        result5 = cursor.fetchone()
+        print(result5)
+        return result5
+
+def updateUserInfo(nq,new_coins,new_life,user_id):
+    with connection.cursor() as cursor:
+        sql4 = "UPDATE users SET current_question='{}',gold_state='{}',life_state='{}' WHERE user_id='{}'".format(nq,
+                                                                                                                  new_coins,
+                                                                                                                  new_life,
+                                                                                                                  user_id)
+
+
+        cursor.execute(sql4)
+        connection.commit()
 
 @route("/start", method="POST")
 def start():
@@ -15,60 +124,21 @@ def start():
     current_adv_id = request.POST.get("adventure")
     print(username)
     current_story_id = 0
-    next_steps_results = [
-        {"option_id": 1, "option_text": "I fight it"},
-        {"option_id": 2, "option_text": "I tell it that I just want to go home"},
-        {"option_id": 3, "option_text": "I give him 10 coins"},
-        {"option_id": 4, "option_text": "I run away quickly"}
-    ]
 
 
-
-    # connection = pymysql.connect(host="us-cdbr-iron-east-04.cleardb.net",
-    #                              user="b69ece0fa2a81b",
-    #                              password="58cea4d5",
-    #                              db="heroku_d958e16ebe26a90",
-    #                              charset="utf8",
-    #                              cursorclass=pymysql.cursors.DictCursor)
-
-    connection = pymysql.connect(host="localhost",
-                                 user="root",
-                                 password="",
-                                 db="adventure",
-                                 charset="utf8",
-                                 cursorclass=pymysql.cursors.DictCursor)
 
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM users WHERE username='{}'".format(username)
-        cursor.execute(sql)
-        user_info = cursor.fetchone()
-        print(user_info)
+
+        user_info=selectAllUserInfoFromName(username)
 
         if user_info:
             print("in")
             user_id = user_info["user_id"]
             print(user_id)
 
-            sql2 = "SELECT current_question FROM users WHERE user_id='{}'".format(user_id)
-            cursor.execute(sql2)
-            cq = cursor.fetchone()
-            question_id = cq["current_question"]
-            print("the current question of the user is")
-            print(question_id)
-
-            sql3 = "SELECT question_text from questions where question_id='{}'".format(question_id)
-            cursor.execute(sql3)
-            result3 = cursor.fetchone()
-            text = result3["question_text"]
-            print(text)
-
-            sql4 = "SELECT option_id,option_text from options where question_id='{}' ORDER BY option_id ASC".format(
-                question_id)
-            cursor.execute(sql4)
-            result4 = cursor.fetchall()
-            print(result4)
-
-            next_steps_results = result4
+            question_id=selectCurrentQuestion(user_id)
+            text=selectQuestionText(question_id)
+            next_steps_results = selectQuestionOptions(question_id)
             print(type(question_id))
 
             return json.dumps({"user": user_id,
@@ -85,12 +155,17 @@ def start():
             cursor.execute(sql, username)
             connection.commit()
             user_id = "null"
+
+
+            text=selectQuestionText(1)
+            options=selectQuestionOptions(1)
+
             return json.dumps({"user": user_id,
                                "adventure": current_adv_id,
                                "current": current_story_id,
-                               "text": "You meet a mysterious creature in the woods, what do you do?",
+                               "text": text,
                                "image": "troll.png",
-                               "options": next_steps_results,
+                               "options": options,
                                "question": 1,
                                "username": username
                                })
@@ -100,7 +175,6 @@ def start():
 def story():
     user_id = request.POST.get("user")
     username = request.POST.get("username")
-
     current_adv_id = request.POST.get("adventure")
     next_story_id = request.POST.get("next")  # this is what the user chose - use it!
     question_id = request.POST.get("question_id")
@@ -108,78 +182,29 @@ def story():
     print(next_story_id)
     end_game=False
 
-    # connection = pymysql.connect(host="us-cdbr-iron-east-04.cleardb.net",
-    #                              user="b69ece0fa2a81b",
-    #                              password="58cea4d5",
-    #                              db="heroku_d958e16ebe26a90",
-    #                              charset="utf8",
-    #                              cursorclass=pymysql.cursors.DictCursor)
-
-
-    connection = pymysql.connect(host="localhost",
-                                 user="root",
-                                 password="",
-                                 db="adventure",
-                                 charset="utf8",
-                                 cursorclass=pymysql.cursors.DictCursor)
-
     with connection.cursor() as cursor:
         if user_id == "null":
-            sql7 = "SELECT user_id from users WHERE username='{}'".format(username)
-            cursor.execute(sql7)
-            result7 = cursor.fetchone()
-            user_id = result7["user_id"]
-            print(result7)
+            user_id=selectUserIdFromName(username)
 
-        sql = "SELECT next_question_id from options WHERE question_id='{}' and option_id='{}'".format(question_id,
-                                                                                                      next_story_id)
-        cursor.execute(sql)
-        result = cursor.fetchone()
-        print(result["next_question_id"])
-        nq = result["next_question_id"]
-
-        sql2 = "SELECT question_text from questions where question_id='{}'".format(result["next_question_id"])
-        cursor.execute(sql2)
-        result2 = cursor.fetchone()
-        print(result2["question_text"])
-        text = result2["question_text"]
-
-        sql3 = "SELECT option_id,option_text from options where question_id='{}'".format(result["next_question_id"])
-        cursor.execute(sql3)
-        result3 = cursor.fetchall()
-
-        sql6 = "SELECT * from users where user_id='{}'".format(user_id)
-        cursor.execute(sql6)
-        result6 = cursor.fetchone()
-        print(result6)
-        print(result6["gold_state"])
-        print(result6["life_state"])
-
-        ucoins = result6["gold_state"]
-        ulife = result6["life_state"]
+        nq=selectNextQuestionId(question_id,next_story_id)
+        text=selectQuestionText(nq)
+        options=selectQuestionOptions(nq)
+        user_info=selectAllUserInfoFromID(user_id)
+        ucoins = user_info["gold_state"]
+        ulife = user_info["life_state"]
 
         print(ucoins)
         print(ulife)
 
-        sql5 = "SELECT gold_change,life_change from options where question_id='{}' and option_id='{}'".format(
-            question_id, next_story_id)
-        cursor.execute(sql5)
-        result5 = cursor.fetchone()
-        print(result5)
-        coins = result5["gold_change"]
-        life = result5["life_change"]
+
+        coins = selectGoldLifeChanges(question_id,next_story_id)["gold_change"]
+        life = selectGoldLifeChanges(question_id,next_story_id)["life_change"]
 
         new_coins = ucoins - coins
         new_life = ulife - life
         print(new_coins)
         print(new_life)
-
-        sql4 = "UPDATE users SET current_question='{}',gold_state='{}',life_state='{}' WHERE user_id='{}'".format(nq,
-                                                                                                                  new_coins,
-                                                                                                                  new_life,
-                                                                                                                  user_id)
-        cursor.execute(sql4)
-        connection.commit()
+        updateUserInfo(nq,new_coins,new_life,user_id)
 
 
         if int(question_id)==9 or int(question_id)==10:
@@ -192,12 +217,13 @@ def story():
 
 
 
+
         return json.dumps({"user": user_id,
                            "adventure": current_adv_id,
                            "question": nq,
                            "text": text,
                            "image": "choice.jpg",
-                           "options": result3,
+                           "options": options,
                            "coins": new_coins,
                            "life": new_life,
                            "loose":end_game
@@ -220,8 +246,8 @@ def images(filename):
 
 
 def main():
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    #run(host="localhost",port="9000")
+    # run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    run(host="localhost",port="9000")
 
 if __name__ == '__main__':
     main()
